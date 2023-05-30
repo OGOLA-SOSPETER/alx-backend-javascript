bcb992e11a5b4a0c6ebe18921995b4bff73637d7
root <captainsos483@gmail.com>
2023-05-30 15:22:17 +0000

    More files

--- 0x06-unittests_in_js/2-calc_chai.js
+++ 0x06-unittests_in_js/2-calc_chai.js
@@ -1,23 +1,18 @@
 function calculateNumber(type, a, b) {
     const an = Math.round(a);
     const bn = Math.round(b);
-    let c = 0;
-    switch (type) {
-    case 'SUM':
-	c = an + bn;
-	break;
-    case 'SUBTRACT':
-	c = an - bn;
-	break;
-    case 'DIVIDE':
-      if (bn === 0) {
-            c = "Error";
-      } else {
-            c = an / bn;
-      }
-      break;
-    }
-    return c;
+	if (type == "SUM"){
+		return an + bn
+	}
+	if (type == "SUBTRACT"){
+		return an - bn
+	}
+	if (type == "DIVIDE"){
+		if (bn == 0){
+			return "Error"
+		}
+		return an / bn
+	}
 }
 
 module.exports = calculateNumber;
