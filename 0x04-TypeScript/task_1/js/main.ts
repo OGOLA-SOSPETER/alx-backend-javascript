e87b3141236afa6d34d66556ad8347f09af77cd6
root <captainsos483@gmail.com>
2023-05-31 08:35:41 +0000

    The print Teacher function

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+interface Teacher{
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: Number,
+  location: string,
+  [propName: string]: any,
+  }
+ 
+interface Directors extends Teacher{
+  numberOfReports: number
+  }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;  
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}.${lastName}`;
+interface classInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
